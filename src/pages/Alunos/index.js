1079b8b000182dbb7d38610328f0383bc60eb3d0
Larissa Chiaradia Da Silva <larissa.silva@fcamara.com>
2024-05-15 19:59:13 -0300

    feat: Implementado o consumo de Api para Alunos

--- src/pages/Alunos/index.js
+++ src/pages/Alunos/index.js
@@ -1,17 +1,50 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import {React, useState, useEffect} from "react";
+import {Link, useNavigate} from "react-router-dom";
 import "./styles.css";
+import api from '../../services/api';
 import logoCadastro from "../../assets/Cadastro.png"
 import { FiEdit, FiXCircle, FiUserX} from "react-icons/fi";
 
 export default function Alunos(){
+
+    const [nome, setNome] = useState('');
+    const [alunos, setAlunos] = useState([]);
+
+    const email = localStorage.getItem("email");
+    const token = localStorage.getItem("token");
+
+    const navigate = useNavigate();
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    useEffect(()=> {
+        api.get("api/Alunos", authorization).then(
+            response=>{setAlunos(response.data);       
+            }, token)
+    });
+
+    async function logout(){
+        try {
+            localStorage.clear();
+            localStorage.setItem("token", "");
+            authorization.headers = "";
+            navigate("/");
+        } catch (error) {
+            alert("Não foi possível fazer o logout" + error);
+        }
+    }
+
     return(
         <div className="aluno-container">
             <header>
                 <img src={logoCadastro} alt="Cadastro"className="logo"/>
-                <span>Bem-Vindo(a), <strong>Larissa</strong>!</span>
+                <span>Bem-Vindo(a), <strong>{email}</strong>!</span>
                 <Link className="button" to="/aluno/novo/0">Novo Aluno</Link>
-                <button type="button">
+                <button onClick={logout} type="button">
                     <FiXCircle size={35} color="#17202a"></FiXCircle>
                 </button>
             </header>
@@ -23,28 +56,21 @@ export default function Alunos(){
             </form>
             <h1>Relação de Alunos</h1>
             <ul>
-                <li>
-                    <b>Nome: </b>Paulo<br/><br/>
-                    <b>Email: </b>paulo@gmail.com<br/><br/>
-                    <b>Idade: </b>22<br/><br/>
+                {alunos.map(aluno=>
+                 <li key={aluno.alunoId}>
+                    <b>Nome: </b>{aluno.nome}<br/><br/>
+                    <b>Email: </b>{aluno.email}<br/><br/>
+                    <b>Idade: </b>{aluno.idade}<br/><br/>
+
                     <button type="button" >
-                        <FiEdit size={25} color="#17202a"></FiEdit>
+                    <FiEdit size={25} color="#17202a"></FiEdit>
                     </button>
                     <button type="button">
-                        <FiUserX size={25} color="#17202a"></FiUserX>
+                    <FiUserX size={25} color="#17202a"></FiUserX>
                     </button>
                 </li>
-                <li>
-                    <b>Nome: </b>Paulo<br/><br/>
-                    <b>Email: </b>paulo@gmail.com<br/><br/>
-                    <b>Idade: </b>22<br/><br/>
-                    <button type="button">
-                        <FiEdit size={25} color="#17202a"></FiEdit>
-                    </button>
-                    <button type="button">
-                        <FiUserX size={25} color="#17202a"></FiUserX>
-                    </button>
-                </li>
+                )}
+
             </ul>
         </div>
     );
